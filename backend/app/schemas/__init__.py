5fb1ade4418a9152e878a0de22c91d2d820dc59c
Fabricio_Ramos <rframosyataco8@gmail.com>
2026-09-12 09:36:24 -0500

    feat: schemas package

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,4 @@
+from app.schemas.user import UserCreate, UserOut, UserLogin, Token
+from app.schemas.catalog import ProductCreate, ProductOut, OriginCreate, OriginOut
+from app.schemas.lot import LotCreate, LotOut, LotOriginCreate
+from app.schemas.sample import SampleLotCreate, SampleLotOut, SampleGrainCreate, SampleGrainOut
